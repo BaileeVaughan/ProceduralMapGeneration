be49ab8614a6319b952d7ff9b17b44ff6eda495c
Bailee Vaughan <47824584+BaileeVaughan@users.noreply.github.com>
2019-09-04 15:00:03 +1000

    Initial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(MazeConstructor))]
+public class GameController : MonoBehaviour
+{
+    private MazeConstructor mC;
+
+    private void Start()
+    {
+        mC = GetComponent<MazeConstructor>();
+        mC.GenerateNewMaze(13, 15);
+    }
+}
